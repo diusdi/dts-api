1a66e0fc8a29759366f8b476f2657d1b46d7b89a
Dion <riadidion@gmail.com>
2023-09-30 06:38:07 +0700

    menambahkan api untuk booking

---
+++ API/Controllers/BookingController.cs
@@ -0,0 +1,77 @@
+﻿using API.Contracts;
+using API.Models;
+using API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BookingController : ControllerBase
+{
+    private readonly IBookingRepository _bookingRepository;
+
+    public BookingController(IBookingRepository bookingRepository)
+    {
+        _bookingRepository = bookingRepository;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var result = _bookingRepository.GetAll();
+        if (!result.Any())
+        {
+            return NotFound("Data Not Found");
+        }
+
+        return Ok(result);
+    }
+
+    [HttpGet("{guid}")]
+    public IActionResult GetByGuid(Guid guid)
+    {
+        var result = _bookingRepository.GetByGuid(guid);
+        if (result is null)
+        {
+            return NotFound("Id Not Found");
+        }
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public IActionResult Create(Booking booking)
+    {
+        var result = _bookingRepository.Create(booking);
+        if (result is null)
+        {
+            return BadRequest("Failed to create data");
+        }
+
+        return Ok(result);
+    }
+
+    [HttpPut]
+    public IActionResult Update(Booking booking)
+    {
+        var result = _bookingRepository.Update(booking);
+        if (!result)
+        {
+            return BadRequest("Failed to update data");
+        }
+
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(Booking booking)
+    {
+        var result = _bookingRepository.Delete(booking);
+        if (!result)
+        {
+            return BadRequest("Failed to delete data");
+        }
+
+        return Ok(result);
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddScoped<IUniversityRepository, UniversityRepository>();
 builder.Services.AddScoped<IEducationRepository, EducationRepository>();
 builder.Services.AddScoped<IAccountRepository, AccountRepository>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IBookingRepository, BookingRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
