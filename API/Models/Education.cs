2a59460f514978de4abc3df9550978d39105305f
Dion <riadidion@gmail.com>
2023-09-29 07:46:58 +0700

    menambahkan migration

--- API/Models/Education.cs
+++ API/Models/Education.cs
@@ -7,7 +7,7 @@ namespace API.Models
     {
         [Column("major", TypeName = "nvarchar(100)")]
         public string Major { get; set; }
-        [Column("degree", TypeName = "nvarchar(100")]
+        [Column("degree", TypeName = "nvarchar(100)")]
         public string Degree { get; set; }
         [Column("gpa")]
         public float Gpa { get; set; }
