29be269ac5bda1d8a00bd60435aa25bf59be744e
Dion <riadidion@gmail.com>
2023-10-04 23:30:26 +0700

    create room validator

---
+++ API/Validations/Rooms/CreateRoomValidator.cs
@@ -0,0 +1,19 @@
+﻿using API.DTOs.Rooms;
+using FluentValidation;
+
+namespace API.Utilities.Validations.Rooms;
+
+public class CreateRoomValidator : AbstractValidator<CreateRoomDto>
+{
+    public CreateRoomValidator()
+    {
+        RuleFor(e => e.Name)
+           .NotEmpty().WithMessage("Tidak Boleh Kosong");
+
+        RuleFor(e => e.Floor)
+           .NotEmpty().WithMessage("Tidak Boleh Kosong");
+
+        RuleFor(e => e.Capacity)
+           .NotEmpty().WithMessage("Tidak Boleh Kosong");
+    }
+}
