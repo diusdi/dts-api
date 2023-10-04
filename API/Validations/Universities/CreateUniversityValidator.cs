9b8136b831d3c6df97aef5f648803b5b8505e91f
Dion <riadidion@gmail.com>
2023-10-04 23:33:29 +0700

    create university validator

---
+++ API/Validations/Universities/CreateUniversityValidator.cs
@@ -0,0 +1,18 @@
+﻿using API.DTOs.Universities;
+using FluentValidation;
+
+namespace API.Utilities.Validations.Universitys;
+
+public class CreateUniversityValidator : AbstractValidator<CreateUniversityDto>
+{
+    public CreateUniversityValidator()
+    {
+        RuleFor(e => e.Name)
+           .NotEmpty().WithMessage("Tidak Boleh Kosong");
+
+        RuleFor(e => e.Code)
+           .NotEmpty().WithMessage("Tidak Boleh Kosong")
+           .MaximumLength(5).WithMessage("Tidak Boleh Melebihi 5 Karakter");
+
+    }
+}
