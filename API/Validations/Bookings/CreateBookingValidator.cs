90d861198438032426c7c00384fc44087a3cc8d0
Dion <riadidion@gmail.com>
2023-10-04 23:13:41 +0700

    create booking validator

---
+++ API/Validations/Bookings/CreateBookingValidator.cs
@@ -0,0 +1,28 @@
+﻿using API.DTOs.Bookings;
+using FluentValidation;
+
+namespace API.Utilities.Validations.Bookings;
+
+public class CreateBookingValidator : AbstractValidator<CreateBookingDto>
+{
+    public CreateBookingValidator()
+    {
+        RuleFor(e => e.StartDate)
+           .NotEmpty().WithMessage("Tidak Boleh Kosong");
+
+        RuleFor(e => e.EndDate)
+           .NotEmpty().WithMessage("Tidak Boleh Kosong");
+
+        RuleFor(e => e.Status)
+           .NotEmpty().WithMessage("Tidak Boleh Kosong");
+
+        RuleFor(e => e.Remarks)
+            .NotEmpty().WithMessage("Tidak Boleh Kosong");
+
+        RuleFor(e => e.RoomGuid)
+           .NotEmpty().WithMessage("Tidak Boleh Kosong");
+
+        RuleFor(e => e.EmployeeGuid)
+           .NotEmpty().WithMessage("Tidak Boleh Kosong");
+    }
+}
