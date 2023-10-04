6860156ff275ef7d4d7fcc2cb7f8c97f3ebde1c8
Dion <riadidion@gmail.com>
2023-10-04 22:37:08 +0700

    create account validation

---
+++ API/Validations/Accounts/CreateAccountValidator.cs
@@ -0,0 +1,23 @@
+﻿using API.DTOs.Accounts;
+using FluentValidation;
+
+namespace API.Utilities.Validations.Accounts;
+
+public class CreateAccountValidator : AbstractValidator<CreateAccountDto>
+{
+    public CreateAccountValidator()
+    {
+        RuleFor(e => e.Password)
+           .NotEmpty()
+           .MinimumLength(8);
+
+        RuleFor(e => e.Otp)
+           .NotEmpty();
+
+        RuleFor(e => e.IsUsed)
+           .NotEmpty()
+           .IsInEnum();
+
+        RuleFor(e => e.ExpiredTime).NotEmpty();
+    }
+}
