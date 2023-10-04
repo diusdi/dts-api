aef1e5cc92262c43b9c6829dc007aac07ebaa4fe
Dion <riadidion@gmail.com>
2023-10-04 23:22:57 +0700

    create education validation

---
+++ API/Validations/Educations/CreateEducationValidator.cs
@@ -0,0 +1,24 @@
+﻿using API.DTOs.Educations;
+using FluentValidation;
+
+namespace API.Utilities.Validations.Educations;
+
+public class CreateEducationValidator : AbstractValidator<CreateEducationDto>
+{
+    public CreateEducationValidator()
+    {
+        RuleFor(e => e.Major)
+           .NotEmpty().WithMessage("Tidak Boleh Kosong");
+
+        RuleFor(e => e.Degree)
+           .NotEmpty().WithMessage("Tidak Boleh Kosong");
+
+        RuleFor(e => e.Gpa)
+           .NotEmpty().WithMessage("Tidak Boleh Kosong")
+           .LessThanOrEqualTo(4).WithMessage("Tidak Boleh Melebihi 4.0");
+
+        RuleFor(e => e.UniversityGuid)
+            .NotEmpty().WithMessage("Tidak Boleh Kosong");
+
+    }
+}
